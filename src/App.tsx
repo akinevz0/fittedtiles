6cc14ce78dbc8fbd64b0a253eea4a3518b0f759c
Kirill kine Nevzorov <akinevz@outlook.com>
2024-09-07 22:07:43 +0000

    add under construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css';
-import BottomBar from './components/BottomBar.tsx';
-import RenderingSection from './components/RenderingSection.tsx';
-import SideBar from './components/SideBar.tsx';
-import MainSection from './components/MainSection.tsx'
+// import BottomBar from './components/BottomBar.tsx';
+// import RenderingSection from './components/RenderingSection.tsx';
+// import SideBar from './components/SideBar.tsx';
+// import MainSection from './components/MainSection.tsx'
 
 const App = () => {
   return <section className='h-full w-full flex'>
-    <SideBar />
-    <MainSection >
-      <RenderingSection />
-      <BottomBar />
-    </MainSection>
+    <div className='place-self-center text-center text-3xl'>
+      Under Construction
+    </div>
   </section>
+
+  // return <section className='h-full w-full flex'>
+  //   <SideBar />
+  //   <MainSection >
+  //     <RenderingSection />
+  //     <BottomBar />
+  //   </MainSection>
+  // </section>
 };
 
 export default App;
